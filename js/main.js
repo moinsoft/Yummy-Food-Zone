f45f4301549b9dd5979cdae60b21cfe5aa7df107
moinsoft <mmoin.official@gmail.com>
2022-01-02 15:54:00 +0600

    One Page Nav Plugin Added

--- js/main.js
+++ js/main.js
@@ -21,13 +21,9 @@ $(window).on('scroll', function () {
   // Sticky Navbar Active
 
   if ($(this).scrollTop() > 25) {
-
     $('.header').addClass('sticky');
-
   } else {
-
     $('.header').removeClass('sticky');
-
   }
 
 
