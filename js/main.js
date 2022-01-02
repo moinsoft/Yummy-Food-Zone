9bf6934b7eb596b7c04e88d38f47ab3476a2a6b9
moinsoft <mmoin.official@gmail.com>
2022-01-02 15:43:42 +0600

    Sticky Navbar Actived

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
 
 
 });
+
+
+$(window).on('scroll', function () {
+
+  // Sticky Navbar Active
+
+  if ($(this).scrollTop() > 25) {
+
+    $('.header').addClass('sticky');
+
+  } else {
+
+    $('.header').removeClass('sticky');
+
+  }
+
+
+});
